d6f02bcb9240573966e98bd6c6725c8bf85f29bb
Sebastian Arango <sebastian.x.arango.-nd@disney.com>
2023-09-26 12:00:40 -0500

    Add screen Nueva Alarma

--- app/src/main/java/com/example/healthlarm/MainActivity.kt
+++ app/src/main/java/com/example/healthlarm/MainActivity.kt
@@ -6,6 +6,6 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_noalarmas)
+        setContentView(R.layout.activity_nuevaalarma)
     }
 }
